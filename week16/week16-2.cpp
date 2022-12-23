0af7917cc7d1f9199d716ebe383ecf3aa0ae4c4b
yuanchicheng <kittycheng1212@gmail.com>
2022-12-23 12:10:03 +0800

    week16

---
+++ week16/week16-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char line[20]="Hello";
+    int N = strlen(line);
+
+
+    printf("Hello¦r¦êªºªø«×:%d\n", N);
+
+    char line2[20];
+    strcpy( line2, line );
+}
